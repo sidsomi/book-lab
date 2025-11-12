91122a51c6365c337b8aa6e90550727036e53dd0
sidsomi <sidbond80@gmail.com>
2025-11-12 12:04:19 -0800

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ class App {
   public static void main(String[] args) {
     Book aBook = new Book();
     System.out.println(aBook.pigLatin("hello"));
-    System.out.println(aBook.translateWord(""));
-  }
-}
+    System.out.println(aBook.translateWord("Allons-y"));
+    System.out.println(aBook.translateSentence("I can now speak in Pig-Latin."));
+}}
