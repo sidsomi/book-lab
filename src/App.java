ed31dc7b6d43d93f071fdcaba0d2476df583c74b
sidsomi <sidbond80@gmail.com>
2025-11-19 11:26:46 -0800

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 class App {
   public static void main(String[] args) {
-    Book aBook = new Book();
-    System.out.println(aBook.pigLatin("hello"));
-    System.out.println(aBook.translateWord("Allons-y"));
-    System.out.println(aBook.translateSentence("I can now speak in Pig-Latin."));
+    Book aBook = new Book("https://www.gutenberg.org/cache/epub/6130/pg6130.txt");
+    Book money = new Book("https://www.gutenberg.org/cache/epub/1513/pg1513.txt");
 }}
